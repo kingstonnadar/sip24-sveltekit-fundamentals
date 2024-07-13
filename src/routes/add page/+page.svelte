dcf18502b3f1aa8354283e0b38b784aeaf4f5111
kingstonnadar <nadarkingston096@gmail.com>
2024-07-13 20:50:34 +0530

    svelte fundamentals
    code on svelte fundamentals

---
+++ src/routes/add page/+page.svelte
@@ -0,0 +1,7 @@
+<header class="bg-white py-4 shadow-md sticky top-0 z-10">
+    <div class="container mx-auto px-4 flex justify-between items-center">
+        <h1 class="text-2xl font-bold font-['Comic-Sans-MS']">CraftLab</h1>
+        <a href="/" class="bg-blue-500 text-white font-bold rounded px-4 py-2 hover:bg-blue-700">Home</a>
+    </div>
+</header>
+
